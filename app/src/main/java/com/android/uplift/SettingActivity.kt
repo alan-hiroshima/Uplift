58a93721e02bce7db2a2fdf4eb8bfda1dbe495e9
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-02-19 18:01:34 +0800

    bug fixes

--- app/src/main/java/com/android/uplift/SettingActivity.kt
+++ app/src/main/java/com/android/uplift/SettingActivity.kt
@@ -2,10 +2,6 @@ package com.android.uplift
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SettingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
